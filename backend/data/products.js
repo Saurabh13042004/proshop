0714452b904f7666c342c0be0a0319291839fbf4
Saurabh Shukla <saurabh1258.be21@chitkarauniversity.edu.in>
2024-07-04 02:51:11 +0530

    first commit

---
+++ backend/data/products.js
@@ -0,0 +1,77 @@
+const products = [
+  {
+    name: 'Redmi Note 10 pro',
+    image: 'https://images-cdn.ubuy.co.in/634d2bfdb1db31066f1077e4-xiaomi-redmi-note-10-pro-128gb-8gb-ram.jpg',
+    description:
+      'Redmi note 10 pro which have super amoled display',
+    brand: 'Xiaomi',
+    category: 'Electronics',
+    price: 14999.99,
+    countInStock: 10,
+    rating: 4.5,
+    numReviews: 12,
+  },
+  {
+    name: 'Samsung S21',
+    image: 'https://images-cdn.ubuy.co.in/633aa23aa4150c30fe6e2448-samsung-galaxy-s21-ultra-5g-factory.jpg',
+    description:
+      'Introducing the one of the best camera phone',
+    brand: 'Samsung',
+    category: 'Electronics',
+    price: 38999.99,
+    countInStock: 7,
+    rating: 4.0,
+    numReviews: 8,
+  },
+  {
+    name: 'Realme techlife earbuds',
+    image: 'https://media-ik.croma.com/prod/https://media.croma.com/image/upload/v1711793552/Croma%20Assets/Entertainment/Wireless%20Earbuds/Images/305924_rqfvj3.png?tr=w-600.jpg',
+    description:
+      'One of the best Earbuds with excellent Base and audio',
+    brand: 'Realme',
+    category: 'Electronics',
+    price: 1499.99,
+    countInStock: 5,
+    rating: 3,
+    numReviews: 12,
+  },
+  {
+    name: 'Airdopes 121',
+    image: 'https://rukminim2.flixcart.com/image/750/900/kzsqykw0/earphone-cable-organizer/o/p/t/boat-airdopes-121v2-true-wireless-earbuds-active-black-bharatx-original-imagbph5zahaaznm.jpeg?q=20&crop=false.jpg',
+    description:
+      'Boat launces the 121 pro edition',
+    brand: 'BOAT',
+    category: 'Electronics',
+    price: 999.99,
+    countInStock: 11,
+    rating: 5,
+    numReviews: 12,
+  },
+  {
+    name: 'BeatXP flux',
+    image: 'https://cdn1.smartprix.com/rx-iUmTdKsLC-w1200-h1200/beatxp-flux-smartwat.jpg',
+    description:
+      'Get a better handle on hand and time',
+    brand: 'BeatXP',
+    category: 'Electronics',
+    price: 1199.99,
+    countInStock: 7,
+    rating: 3.5,
+    numReviews: 10,
+  },
+  {
+    name: 'HP laptop Elitebook 745 G6',
+    image: 'https://5.imimg.com/data5/SELLER/Default/2021/3/YE/VN/KU/112920323/hp-laptop-1000x1000.jpg',
+    description:
+      'HP elitebook 745 G6 laptop with best resolution',
+    brand: 'HP',
+    category: 'Electronics',
+    price: 47999.99,
+    countInStock: 0,
+    rating: 4,
+    numReviews: 12,
+  },
+  
+];
+
+export default products;
